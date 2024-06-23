72ac42dd224d6ba35e75b6e01e986de241ca3d93
Andrei <aformusatii@gmail.com>
2024-06-23 23:33:09 +0300

    Auto commit

--- static/js/main.js
+++ static/js/main.js
@@ -20,3 +20,13 @@ const toTrueFalse = function(value) {
     return (value === 'on');
 }
 
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/js/sw.js')
+        .then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function(error) {
+            console.log('Service Worker registration failed:', error);
+        });
+}
